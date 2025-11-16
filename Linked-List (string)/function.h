06ffb0785612a0a7b2a4b3f0fc624dfa43384191
Gian-Joebert-Caparas <gianjoebert.caparas@cit.edu>
2025-11-17 00:17:15 +0800

    Add deleteIndex and deleteAllNodes functions

--- Linked-List (string)/function.h
+++ Linked-List (string)/function.h
@@ -14,7 +14,7 @@ Node* createNode(char text[]);
 void display(Node* head);
 void deleteFirst(Node** head);
 void deleteLast(Node** head);
-void deleteWord(Node** head, );
-
-
+void deleteWord(Node** head, char wordDelete[]);
+void deleteIndex(Node** head, int index);
+void deleteAllNodes(Node** head);
 #endif //PREFINALS_DSA_FUNCTION_H
