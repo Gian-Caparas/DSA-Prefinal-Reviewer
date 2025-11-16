2b3704b236e8115d36c7560c7e50a3ae5f87c796
Gian-Joebert-Caparas <gianjoebert.caparas@cit.edu>
2025-11-17 00:16:55 +0800

    Add functions to delete nodes from linked list

--- Linked-List (string)/function.c
+++ Linked-List (string)/function.c
@@ -6,7 +6,7 @@
 #include <string.h>
 #include "function.h"
 
-Node* createNode(char text[]) {
+Node* createNode(char text[]){
     Node* newNode = malloc(sizeof(Node));
     strcpy(newNode->word, text);
     newNode->next = NULL;
@@ -14,7 +14,7 @@ Node* createNode(char text[]) {
     return newNode;
 }
 
-void display(Node* head) {
+void display(Node* head){
     printf("Original linked-list:\n");
     while (head!=NULL) {
         printf("%s", head->word);
@@ -23,4 +23,86 @@ void display(Node* head) {
         }
         head = head->next;
     }
+}
+
+void deleteFirst(Node** head){
+    if(head == NULL){
+        printf("Empty linked-list\n");
+        return;
+    }
+    Node* current = (*head)->next;
+    free(*head);
+    *head = current;
+}
+
+void deleteLast(Node** head){
+    if(head == NULL){
+        printf("Empty linked-list\n");
+        return;
+    }
+
+    Node* current = *head;
+    Node* prev = NULL;
+    while(current->next!=NULL){   
+        prev = current;
+        current = current->next;
+    }
+
+    prev->next = NULL;
+    free(current);
+}
+
+void deleteWord(Node** head, char wordDelete[]){
+    if(head == NULL){
+        printf("Empty linked-list\n");
+        return;
+    }
+
+    Node* current = *head;
+    Node* prev = NULL;
+
+    while(current!=NULL && strcmp(current->word, wordDelete) != 0){
+        prev = current;
+        current = current->next;
+    }
+
+    prev->next = current->next;
+    free(current);
+}
+
+void deleteIndex(Node** head, int index){
+    if(head == NULL){
+        printf("Empty linked-list\n");
+        return;
+    }
+
+    Node* current = *head;
+    Node* prev = NULL;
+    int count = 1;
+
+    if(index == count){
+        *head = (*head)->next;
+        free(current);
+        return;
+    }
+
+    while(current!=NULL && index != count){
+        prev = current;
+        count++;
+        current = current->next;
+    }
+    prev->next = current->next;
+    free(current);
+}
+
+void deleteAllNodes(Node** head){
+    Node* current = *head;
+    Node* nextNode = NULL;
+    while (current!=NULL) {
+        nextNode = current->next;
+        free(current);
+        current = nextNode;
+    }
+    *head = NULL;
+    printf("All nodes are deleted!");
 }
